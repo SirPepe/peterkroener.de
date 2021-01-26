0fdcd8593decfa7ac2ef4dfe6be70eb7635ffc06
Peter Kröner <peter@peterkroener.de>
2021-01-26 10:21:57 +0100

    Update Dependencies & Warhol

--- snapshots.sh
+++ snapshots.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-node node_modules/.bin/warhol-collect \
+node node_modules/@warhol/collector-service/dist/cjs/cli.js \
     --verbose \
     --patternLibId peterkroener.de \
-    --browser chrome \
     --input warhol.config.json \
-    --output snapshots.chrome.json
+    --output snapshots.json
