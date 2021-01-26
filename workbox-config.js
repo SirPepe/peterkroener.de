88da2b4159615a56ac869bba2dd1883f7e411b9a
Peter Kröner <peter@peterkroener.de>
2021-01-26 10:35:01 +0100

    update workbox

--- workbox-config.js
+++ workbox-config.js
@@ -1,12 +1,15 @@
 module.exports = {
-  "globDirectory": "./out/",
-  "globPatterns": [
+  globDirectory: "./out/",
+  globPatterns: [
     "static/*.js",
     "static/*.css",
     "static/fonts/*.*",
     "static/images/*.*",
   ],
-  "swDest": "./out/sw.js",
-  "clientsClaim": false,
-  "skipWaiting": false
+  swDest: "./out/sw.js",
+  clientsClaim: false,
+  skipWaiting: false,
+  inlineWorkboxRuntime: true,
+  mode: "production",
+  sourcemap: false,
 };
